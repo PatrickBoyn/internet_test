412424020eee2cf0e16bf479ad21a7e1e5fdb7ba
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-05-02 14:07:32 -0400

    This is a simple script I found on Stack Overflow and changed to my own purposes.

---
+++ internet_test.py
@@ -0,0 +1,13 @@
+-import socket
+
+def is_connected(server):
+    try:
+        host = socket.gethostbyname(server)
+        s = socket.create_connection((host, 80), 2)
+        print("There is a connection.")
+        return True
+    except:
+        print("Sorry you are not connected.")
+        return False
+
+print(is_connected('www.yahoo.com'))
